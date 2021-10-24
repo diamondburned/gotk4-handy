7a67085dfa2606b8db69f01b010e3a22310e44ea
diamondburned <diamond@arikawa-hi.me>
2021-10-24 16:14:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# gotk4-handy
+
+[Package documentation](https://pkg.go.dev/github.com/diamondburned/gotk4-handy/pkg/handy)
+
+Go generated bindings for [libhandy][handy] (GTK3).
+
+[handy]: https://gnome.pages.gitlab.gnome.org/libhandy/doc/1-latest/
---
+++ config.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/diamondburned/gotk4/gir/cmd/gir_generate/gendata"
+	"github.com/diamondburned/gotk4/gir/girgen/types"
+)
+
+const (
+	gotk4Module   = "github.com/diamondburned/gotk4/pkg"
+	adwaitaModule = "github.com/diamondburned/gotk4-handy/pkg"
+)
+
+var packages = []gendata.Package{
+	{PkgName: "libhandy-1", Namespaces: nil},
+}
+
+var pkgGenerated = []string{
+	"handy",
+}
+
+var pkgExceptions = []string{
+	"go.mod",
+	"go.sum",
+	"LICENSE",
+	// "_examples",
+}
+
+var preprocessors = []types.Preprocessor{
+	// types.PreprocessorFunc(func(repos gir.Repositories) {
+	// 	layershell := repos.FromPkg("gtk-layer-shell-0")
+	// 	layershell.Packages = append(layershell.Packages, gir.Package{
+	// 		Name: "gtk+-3.0",
+	// 	})
+	// }),
+}
+
+var filters = []types.FilterMatcher{}
